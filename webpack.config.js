75d327955bae335740b6fbbc8953d977f0020463
Krystian <juszczakkrystian@gmail.com>
2022-01-16 15:10:50 +0100

    Added mobile-first approach file schema and responsive images.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+
 const postCSSPlugins = [
   require('postcss-import'),
+  require('postcss-mixins'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
   require('autoprefixer')
